7a4dede3b2e8ab6aa16e5e55693d1eccd29b9eb8
beliemun <beliemun@paran.com>
2021-07-12 18:13:23 +0900

    #16.4 Search part Two

--- src/navigators/AuthTab.tsx
+++ src/navigators/AuthTab.tsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { Image, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AuthTabParamList } from "~/../@types/navigation/auth";
 import { darkTheme, lightTheme } from "@common/theme";
 import { Ionicons } from "@expo/vector-icons";
-import { View } from "react-native";
 import AuthStack from "./AuthStack";
 import { useColorScheme } from "react-native";
+import useLoggedInUser from "~/hooks/useLoggedInUser";
 
 const Tabs = createBottomTabNavigator<AuthTabParamList>();
 
 const Auth: React.FC = () => {
   const colorScheme = useColorScheme();
+  const { data } = useLoggedInUser();
+
+  interface IProps {
+    uri: string;
+    focused: boolean;
+  }
+  const AvatarIcon: React.FC<IProps> = ({ uri, focused }) => (
+    <View
+      style={{
+        width: focused ? 28 : 24,
+        height: focused ? 28 : 24,
+        borderRadius: focused ? 14 : 12,
+        overflow: "hidden",
+        borderWidth: focused ? 2 : 0,
+        borderColor:
+          colorScheme === "light" ? lightTheme.accent : darkTheme.accent,
+      }}
+    >
+      <Image source={{ uri }} style={{ width: "100%", height: "100%" }} />
+    </View>
+  );
+
   return (
     <NavigationContainer>
       <Tabs.Navigator
@@ -86,13 +109,16 @@ const Auth: React.FC = () => {
         <Tabs.Screen
           name="MyProfile"
           options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Ionicons
-                name={focused ? "person" : "person-outline"}
-                color={color}
-                size={24}
-              />
-            ),
+            tabBarIcon: ({ focused, color, size }) =>
+              data?.me?.avatar ? (
+                <AvatarIcon uri={data.me.avatar} focused={focused} />
+              ) : (
+                <Ionicons
+                  name={focused ? "person" : "person-outline"}
+                  color={color}
+                  size={24}
+                />
+              ),
           }}
         >
           {() => <AuthStack screenName={"MyProfile"} />}
--- src/screens/auth/Search/Serach.tsx
+++ src/screens/auth/Search/Serach.tsx
@@ -8,11 +8,17 @@ import {
 import styled from "styled-components/native";
 import RegEx from "@common/rules";
 import { Controller, useForm } from "react-hook-form";
+import { gql, useLazyQuery } from "@apollo/client";
+import { searchPhotos } from "types/__generated__/searchPhotos";
 
-interface IProps {
-  navigation: SearchScreenNavigationProp;
-  route: SearchScreenRouteProp;
-}
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
 
 const TextInput = styled.TextInput``;
 
@@ -21,7 +27,17 @@ interface IFormInput {
   error: string;
 }
 
+interface IProps {
+  navigation: SearchScreenNavigationProp;
+  route: SearchScreenRouteProp;
+}
+
 const Serach: React.FC<IProps> = ({ navigation, route }) => {
+  // useQuery는 컴포넌트에 마운트 될 때 자동으로 호출된다.
+  // skip를 이용해 실행을 막을 수 있지만 Query문 실행 자체를 막을 수 없다.
+  // useLazyQuery는 startQueryFn를 호출하기 전까지 실행되지 않는다.
+  const [startQueryFn, { loading, data }] =
+    useLazyQuery<searchPhotos>(SEARCH_PHOTOS);
   const {
     control,
     handleSubmit,
